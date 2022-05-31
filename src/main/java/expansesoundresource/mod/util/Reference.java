3df259e3d89b28c17ae0824c33cfc8aff2d8198c
Tempest051 <67126196+NoMoreUsernames999@users.noreply.github.com>
2022-05-31 10:37:34 -0400

    Add files via upload

---
+++ src/main/java/expansesoundresource/mod/util/Reference.java
@@ -0,0 +1,12 @@
+package expansesoundresource.mod.util;
+
+
+public class Reference 
+{
+	public static final String MODID = "expansesoundresource";
+	public static final String MODNAME = "Expanse Sound Resources";
+	public static final String VERSION = "1.12.2";
+	
+	public static final String CLIENT = "expansesoundresource.mod.proxy.ClientProxy";
+	public static final String SERVER = "expansesoundresource.mod.proxy.CommonProxy";
+}
